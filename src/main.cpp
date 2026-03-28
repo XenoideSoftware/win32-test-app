f3b0e645de86c209846f57a7ca5c172dfbc1081a
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-03-28 18:51:20 -0300

    coverted to winlamb

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,28 @@
-#include <tchar.h>
-#include <windows.h>
+#include "winlamb/window_main.h"
 
-static const TCHAR* CLASS_NAME = TEXT("ModernWinApp");
+class My_Window : public wl::window_main {
+public:
+    My_Window();
+};
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+RUN(My_Window) // optional, generate WinMain call and instantiate My_Window
+
+My_Window::My_Window()
 {
-    switch (msg)
-    {
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        DrawText(hdc, TEXT("Hello, Win32!"), -1, &ps.rcPaint, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-        EndPaint(hwnd, &ps);
-        return 0;
-    }
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
-}
+    setup.wndClassEx.lpszClassName = L"SOME_CLASS_NAME"; // class name to be registered
+    setup.title = L"This is my window";
+    setup.style |= WS_MINIMIZEBOX;
 
-int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int nCmdShow)
-{
-    WNDCLASSEX wc    = {};
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.lpfnWndProc   = WndProc;
-    wc.hInstance     = hInstance;
-    wc.hCursor       = LoadCursor(nullptr, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszClassName = CLASS_NAME;
-    RegisterClassEx(&wc);
+    on_message(WM_CREATE, [this](wl::wm::create p) -> LRESULT
+        {
+            set_text(L"A new title for the window");
+            return 0;
+        });
 
-    HWND hwnd = CreateWindowEx(
-        0, CLASS_NAME, TEXT("Modern Win32 App"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
-        nullptr, nullptr, hInstance, nullptr
-    );
-
-    if (!hwnd) return 1;
-
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-
-    MSG msg = {};
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return (int)msg.wParam;
+    on_message(WM_LBUTTONDOWN, [](wl::wm::lbuttondown p) -> LRESULT
+        {
+            bool isCtrlDown = p.has_ctrl();
+            long xPos = p.pos().x;
+            return 0;
+        });
 }
