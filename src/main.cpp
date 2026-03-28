370193fd3bda1ad3ef6cf5399f6c1d540d50fcad
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-03-28 20:22:28 -0300

    added command

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "winlamb/window_main.h"
+#include "winlamb/button.h"
 
 class My_Window : public wl::window_main {
 public:
     My_Window();
+
+private:
+    wl::button my_button;
 };
 
 RUN(My_Window) // optional, generate WinMain call and instantiate My_Window
@@ -16,13 +20,23 @@ My_Window::My_Window()
     on_message(WM_CREATE, [this](wl::wm::create p) -> LRESULT
         {
             set_text(L"A new title for the window");
+            my_button.create(this, 10000, L"Test", POINT{ 0, 0 }, SIZE{640, 50});
             return 0;
         });
+
+
+    on_command(10001 , [this](wl::wm::command p) -> LRESULT {
+        MessageBox(NULL, L"Text", L"Caption", MB_OK);
+
+        return 0;
+    });
 
     on_message(WM_LBUTTONDOWN, [](wl::wm::lbuttondown p) -> LRESULT
         {
             bool isCtrlDown = p.has_ctrl();
             long xPos = p.pos().x;
+            
+
             return 0;
         });
 }
