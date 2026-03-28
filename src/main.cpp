2e62f7aff327bbf2178bca111f9d3f0529c186e2
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-03-28 18:12:08 -0300

    exported windows projects

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
+#include <tchar.h>
 #include <windows.h>
 
-static const wchar_t* CLASS_NAME = L"ModernWinApp";
+static const TCHAR* CLASS_NAME = TEXT("ModernWinApp");
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -11,7 +12,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        DrawTextW(hdc, L"Hello, Win32!", -1, &ps.rcPaint, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        DrawText(hdc, TEXT("Hello, Win32!"), -1, &ps.rcPaint, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
         EndPaint(hwnd, &ps);
         return 0;
     }
@@ -19,22 +20,22 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         PostQuitMessage(0);
         return 0;
     }
-    return DefWindowProcW(hwnd, msg, wParam, lParam);
+    return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int nCmdShow)
 {
-    WNDCLASSEXW wc = {};
-    wc.cbSize        = sizeof(WNDCLASSEXW);
+    WNDCLASSEX wc    = {};
+    wc.cbSize        = sizeof(WNDCLASSEX);
     wc.lpfnWndProc   = WndProc;
     wc.hInstance     = hInstance;
     wc.hCursor       = LoadCursor(nullptr, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.lpszClassName = CLASS_NAME;
-    RegisterClassExW(&wc);
+    RegisterClassEx(&wc);
 
-    HWND hwnd = CreateWindowExW(
-        0, CLASS_NAME, L"Modern Win32 App",
+    HWND hwnd = CreateWindowEx(
+        0, CLASS_NAME, TEXT("Modern Win32 App"),
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         nullptr, nullptr, hInstance, nullptr
@@ -46,10 +47,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
     UpdateWindow(hwnd);
 
     MSG msg = {};
-    while (GetMessageW(&msg, nullptr, 0, 0))
+    while (GetMessage(&msg, nullptr, 0, 0))
     {
         TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+        DispatchMessage(&msg);
     }
 
     return (int)msg.wParam;
