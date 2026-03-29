d69383cea7e6de634dd332c52099176bc66b11a2
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-03-29 10:32:31 -0300

    added windows resource

---
+++ src/resource.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define APP_VERSION_MAJOR   1
+#define APP_VERSION_MINOR   0
+#define APP_VERSION_PATCH   0
+#define APP_VERSION_BUILD   0
+
+#define APP_VERSION_STR     "1.0.0.0"
+#define APP_VERSION_WSTR    L"1.0.0.0"
+
+#define APP_PRODUCT_NAME    "ModernWinApp"
+#define APP_DESCRIPTION     "Modern Windows Application"
+#define APP_COMPANY_NAME    "NativeDevCL"
+#define APP_COPYRIGHT       "Copyright (C) 2026 NativeDevCL"
